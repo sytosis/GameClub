4087e808133143fa77ad404e858fea40693a6344
R_O_B <robert.francis@uqconnect.edu.au>
2020-09-06 13:17:29 +1000

    Work

--- app/src/main/java/com/example/gameclub/Games/Bingo/Bingo.java
+++ app/src/main/java/com/example/gameclub/Games/Bingo/Bingo.java
@@ -1,4 +1,59 @@
 package com.example.gameclub.Games.Bingo;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.lifecycle.ViewModel;
 
-public class Bingo {
+import com.example.gameclub.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Bingo extends ViewModel {
+    public List<Integer> bingoBoard = new ArrayList<>(25);
+
+    List<Integer> usedNumbers = new ArrayList<>();
+    Random randomGenerator = new Random();
+
+    public Bingo() {
+        setBoard();
+    }
+
+    public void setBoard() {
+        int number;
+        for (int i = 0; i < 25; ++i) {
+
+            while(!bingoBoard.contains(number = randomGenerator.nextInt(100) + 1)) {
+                bingoBoard.add(number);
+                break;
+            }
+        }
+    }
+
+    public int getBall() {
+        int number;
+        while(!bingoBoard.contains(number = randomGenerator.nextInt(100) + 1)) {
+            return number;
+        }
+        return 0;
+    }
+
+    public void takeTurn() {
+        int ball = getBall();
+
+//        if (bingoBoard.contains(ball)) {
+//
+//        }
+    }
+
+    public void reset() {
+        bingoBoard.clear();
+        usedNumbers.clear();
+
+    }
+
+    public boolean checkWin() {
+        return false;
+    }
 }
--- app/src/main/java/com/example/gameclub/Games/Bingo/BingoFragment.java
+++ app/src/main/java/com/example/gameclub/Games/Bingo/BingoFragment.java
@@ -1,4 +1,94 @@
 package com.example.gameclub.Games.Bingo;
 
-public class BingoFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.gameclub.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BingoFragment extends Fragment {
+
+    private Bingo bingoGame;
+    List<ImageView> bingoBoardImages = new ArrayList<>(25);
+
+    public void fillBingoBoardImageList(View in) {
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball1));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball2));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball3));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball4));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball5));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball6));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball7));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball8));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball9));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball10));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball11));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball12));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball13));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball14));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball15));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball16));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball17));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball18));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball19));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball20));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball21));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball22));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball23));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball24));
+        bingoBoardImages.add((ImageView) in.findViewById(R.id.Ball25));
+    }
+
+    public void setBoardColours() {
+
+//        bingoBoardImages.get(0).setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.red_ball, null));
+
+//        int number;
+//        for (int i = 0; i < 25; ++i) {
+//            number = bingoGame.bingoBoard.get(i);
+//
+//            System.out.println("LOOP");
+//
+//            if (number < 16) {
+//                bingoBoardImages.get(i).setImageDrawable();
+//            } else if (number < 31) {
+//                bingoBoardImages.get(i).setImageResource(R.drawable.blue_ball);
+//            } else if (number < 45) {
+//                bingoBoardImages.get(i).setImageResource(R.drawable.yellow_ball);
+//            } else if (number < 61) {
+//                bingoBoardImages.get(i).setImageResource(R.drawable.green_ball);
+//            } else {
+//                bingoBoardImages.get(i).setImageResource(R.drawable.purple_ball);
+//            }
+//        }
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        bingoGame = ViewModelProviders.of(this).get(Bingo.class);
+
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        fillBingoBoardImageList(root);
+        setBoardColours();
+
+        Button rollBall = root.findViewById(R.id.roll_ball_button);
+        Button resetButton = root.findViewById(R.id.new_list_button);
+
+        return root;
+    }
 }
