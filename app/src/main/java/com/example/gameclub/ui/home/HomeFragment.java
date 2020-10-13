b1aa830f47e2c655e02861b422d84f108b209d24
Kevin <kevinchua88@yahoo.com.sg>
2020-10-13 22:07:22 +1000

    ui changes

--- app/src/main/java/com/example/gameclub/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/gameclub/ui/home/HomeFragment.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -21,35 +22,43 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     Button gameButton;
-    Button friendButton;
-    Button chessButton;
+    Button leftButton;
+    Button rightButton;
+    Button profileButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView homeText= root.findViewById(R.id.text_home);
         gameButton = root.findViewById(R.id.game_button);
-        friendButton = root.findViewById(R.id.friend_button);
-        chessButton = root.findViewById(R.id.chess_button);
+        leftButton = root.findViewById(R.id.left_button);
+        rightButton = root.findViewById(R.id.right_button);
+        profileButton = root.findViewById(R.id.profile_button);
+        //chessButton = root.findViewById(R.id.chess_button);
         final MainActivity main = (MainActivity) requireActivity();
         gameButton.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
-                  NavHostFragment.findNavController(HomeFragment.this).navigate((R.id.action_nav_home_to_nav_gallery));
+                  leftButton.setBackground(AppCompatResources.getDrawable(getContext(),R.drawable.bingo_button));
+                  rightButton.setBackground(AppCompatResources.getDrawable(getContext(),R.drawable.chess_button));
+                  profileButton.setVisibility(View.INVISIBLE);
+                  gameButton.setVisibility(View.INVISIBLE);
               }
         });
+        /*
         chessButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(HomeFragment.this).navigate((R.id.action_nav_home_to_nav_chess));
             }
         });
+         */
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                homeText.setText("Hi " + main.getFirstName() + ", what would you like to do?");
             }
         });
         return root;
