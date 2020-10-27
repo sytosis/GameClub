a63493a882e57e68541f248c8f160387016d8e46
j-roby <johnroby1997@gmail.com>
2020-10-27 12:07:37 +1000

    chess networking update

--- app/src/main/java/com/example/gameclub/Network/ChessGame.java
+++ app/src/main/java/com/example/gameclub/Network/ChessGame.java
@@ -1,4 +1,48 @@
 package com.example.gameclub.Network;
 
-public class ChessGame {
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import androidx.appcompat.content.res.AppCompatResources;
+
+import com.example.gameclub.R;
+import com.example.gameclub.Ui.Gallery.ChessViewModel;
+
+public class ChessGame implements Game{
+
+    private ChessViewModel chessViewModel;
+
+    View root;
+    Boolean onWhite = true;
+    public ChessGame(ChessViewModel chessModel,  View rootSave) {
+        chessViewModel = chessModel;
+        root = rootSave;
+    }
+
+    public String gameBoard(){
+        return " ";
+    }
+
+    public void chat(Chat newMessage){
+
+    }
+
+    public void setGame(String string){
+
+    }
+
+    public ChessViewModel getChessViewModel() {
+
+        return chessViewModel;
+    }
+    public String typeof(){
+
+        return "chess";
+    }
+
 }
--- app/src/main/java/com/example/gameclub/Network/ServerNetwork.java
+++ app/src/main/java/com/example/gameclub/Network/ServerNetwork.java
@@ -1,5 +1,6 @@
 package com.example.gameclub.Network;
 
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -7,7 +8,10 @@ import android.widget.TextView;
 
 import com.example.gameclub.R;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,10 +20,10 @@ import java.net.Socket;
 public class ServerNetwork implements Runnable {
     TextView text;
     Socket client;
-    Game bingoGame = new BingoGame();
-    public ServerNetwork(TextView view) {
+    ChessGame chessGame;
+    public ServerNetwork(TextView view, ChessGame newChessGame) {
         text = view;
-
+        chessGame = newChessGame;
     }
 
     public void run() {
@@ -31,14 +35,28 @@ public class ServerNetwork implements Runnable {
             /*Listen for connections */
             while (true) {
                 client = sock.accept();
-                System.out.println("out here");
-                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+                InputStream in = client.getInputStream();
+                BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+                /*read the data from the socket*/
+                String line;
+                while ((line = bin.readLine()) != null) {
+                    String[] arrOfStr = line.split(":",1);
+                    Integer x;
+                    Integer y;
+                    x = Integer.parseInt(arrOfStr[0]);
+                    y = Integer.parseInt(arrOfStr[1]);
+                    chessGame.getChessViewModel().getMove().setValue(Pair.create(x,y));
+                }
+
+                //tem.out.println("out here");
+                //PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
                 /*write the data to the socket*/
-                pout.println();
-                text.setText("Server Connected");
-                System.out.println(client.getPort());
-                System.out.println(client.getLocalPort());
-                pout.println(new java.util.Date().toString());
+                //pout.println();
+                //text.setText("Server Connected");
+                //System.out.println(client.getPort());
+                //System.out.println(client.getLocalPort());
+                //pout.println(new java.util.Date().toString());
                 /*close the socket and resume */
                 /*listening for connections*/
                 client.close();
