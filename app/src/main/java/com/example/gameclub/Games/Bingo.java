bfcd801d564a1ee33685d6505048a7b06930a1ab
jng94 <jordanng11@hotmail.com>
2020-10-27 13:53:23 +1000

    revert + merge

--- app/src/main/java/com/example/gameclub/Games/Bingo.java
+++ app/src/main/java/com/example/gameclub/Games/Bingo.java
@@ -15,8 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 public class Bingo extends ViewModel {
     public List<Integer> bingoBoard = new ArrayList<>(25);
@@ -27,10 +26,6 @@ public class Bingo extends ViewModel {
     private String id;
     private boolean isHost = false;
     private Integer ballNum = 0;
-    private Integer playerNum = 0;
-    private Boolean started = false;
-    private Boolean added = false;
-    private Integer winner = -1;
 
     public Bingo() {
         setBoard();
@@ -44,10 +39,6 @@ public class Bingo extends ViewModel {
                             mDatabase.child("Games").child("Bingo").child("Hosting").child("id").setValue(MainActivity.currentUser.getId());
                             isHost = true;
                         }
-                        playerNum = Integer.parseInt(snapshot.child("Games").child("Bingo").child("Hosting").child("Num").getValue().toString());
-                        playerNum = playerNum + 1;
-                        Log.d("PlayerNum", String.valueOf(playerNum));
-                        mDatabase.child("Games").child("Bingo").child("Hosting").child("Num").setValue(playerNum);
                     } catch (Exception e) {
                         Log.d("Exception: ", String.valueOf(e));
                     }
@@ -68,27 +59,6 @@ public class Bingo extends ViewModel {
                             Log.d("Exception: ", String.valueOf(e));
                         }
                     }
-                }
-                if (isHost && !started) {
-                    for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-                        try {
-                            if (snapshot.child("Bingo").child("Hosting").child("Num").getValue().toString().equals("0")) {
-                                System.out.println("Player num is empty");
-                            }
-                            playerNum = Integer.parseInt(snapshot.child("Bingo").child("Hosting").child("Num").getValue().toString());
-                            if (playerNum == 2) {
-                                startGame();
-                            }
-                            Log.d("PlayerNum ", String.valueOf(playerNum));
-                        } catch (Exception e) {
-                            Log.d("Exception: ", String.valueOf(e));
-                        }
-                    }
-                }
-                winner = (Integer) snapshot.child("Bingo").child("Hosting").child("winner").getValue();
-                if (winner != -1) {
-                    System.out.println("The winner is " + winner);
-                    reset();
                 }
             }
 
@@ -121,18 +91,6 @@ public class Bingo extends ViewModel {
         }
     }
 
-    public void startGame() {
-        started = true;
-        mDatabase.child("Games").child("Bingo").child("Hosting").child("started").setValue("true");
-        playGame();
-    }
-
-    public void playGame() {
-        while(true) {
-            checkWin(getBall());
-        }
-    }
-
     public int getBall() {
         int number = 0;
         if (isHost) {
@@ -150,9 +108,6 @@ public class Bingo extends ViewModel {
         if (isHost) {
             mDatabase.child("Games").child("Bingo").child("Hosting").child("id").setValue("empty");
             mDatabase.child("Games").child("Bingo").child("Hosting").child("Ball").setValue("empty");
-            mDatabase.child("Games").child("Bingo").child("Hosting").child("winner").setValue(-1);
-            mDatabase.child("Games").child("Bingo").child("Hosting").child("started").setValue("false");
-            mDatabase.child("Games").child("Bingo").child("Hosting").child("Num").setValue(0);
         }
         bingoBoard.clear();
         usedNumbers.clear();
@@ -171,8 +126,8 @@ public class Bingo extends ViewModel {
             win = true;
         }
 
-        if (win && winner == -1) {
-            mDatabase.child("Games").child("Bingo").child("Hosting").child("winner").setValue(MainActivity.currentUser.getId());
+        if (win) {
+            reset();
         }
         for (int i = 0; i < 25; ++i) {
             Integer send = bingoBoard.get(i);
