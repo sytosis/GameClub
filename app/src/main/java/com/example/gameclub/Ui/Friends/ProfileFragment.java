357f950c2f8417d8aca53c8620f56b26ee98271a
Kevin <kevinchua88@yahoo.com.sg>
2020-10-28 23:08:31 +1000

    home fixed

---
+++ app/src/main/java/com/example/gameclub/Ui/Friends/ProfileFragment.java
@@ -0,0 +1,30 @@
+package com.example.gameclub.Ui.Friends;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.gameclub.Games.Bingo;
+import com.example.gameclub.Games.BingoFragment;
+import com.example.gameclub.MainActivity;
+import com.example.gameclub.R;
+
+public class ProfileFragment extends Fragment {
+
+    static View root;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return root;
+    }
+}
